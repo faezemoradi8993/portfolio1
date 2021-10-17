c0669b5ce21e52e46923df653ddf462450f97f9e
faeze <77538218+faezemoradi8993@users.noreply.github.com>
2021-10-17 04:07:34 +0330

    Updates

---
+++ precache-manifest.2f4294571cee843ccda4b87d4694e292.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12f57a689fada39104b8295398484884",
+    "url": "/faezemoradi8993.github.io/portfolio1/index.html"
+  },
+  {
+    "revision": "78ec1bd4c43a948fc314",
+    "url": "/faezemoradi8993.github.io/portfolio1/static/js/2.7940b71d.chunk.js"
+  },
+  {
+    "revision": "ea88211925d18270e09b4691b32a2900",
+    "url": "/faezemoradi8993.github.io/portfolio1/static/js/2.7940b71d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc478b7256e5d4a1746e",
+    "url": "/faezemoradi8993.github.io/portfolio1/static/js/main.c0856bf2.chunk.js"
+  },
+  {
+    "revision": "57b64ae4b5061f92f3df",
+    "url": "/faezemoradi8993.github.io/portfolio1/static/js/runtime-main.c778a617.js"
+  },
+  {
+    "revision": "87446ab560154329720d2141f338b678",
+    "url": "/faezemoradi8993.github.io/portfolio1/static/media/avatar.87446ab5.jpg"
+  },
+  {
+    "revision": "219053d2ac6d177919a3183b5d1b2582",
+    "url": "/faezemoradi8993.github.io/portfolio1/static/media/blog1.219053d2.svg"
+  },
+  {
+    "revision": "c76e33c50cc01ffa940bd90d94fb3a03",
+    "url": "/faezemoradi8993.github.io/portfolio1/static/media/blog2.c76e33c5.svg"
+  },
+  {
+    "revision": "d47df84ad387e54d6de8c1d1d2831bf2",
+    "url": "/faezemoradi8993.github.io/portfolio1/static/media/blog3.d47df84a.svg"
+  },
+  {
+    "revision": "273928160386f3b78858d7261fe4ea2e",
+    "url": "/faezemoradi8993.github.io/portfolio1/static/media/blog4.27392816.svg"
+  },
+  {
+    "revision": "3ddaa05763624d6881593d127bfba05d",
+    "url": "/faezemoradi8993.github.io/portfolio1/static/media/design.3ddaa057.svg"
+  },
+  {
+    "revision": "4526f305900c1f3bcf192c10f45e8078",
+    "url": "/faezemoradi8993.github.io/portfolio1/static/media/game-dev.4526f305.svg"
+  },
+  {
+    "revision": "3e3ae13253a1c6d68ced29700bcf4708",
+    "url": "/faezemoradi8993.github.io/portfolio1/static/media/intelligence.3e3ae132.svg"
+  },
+  {
+    "revision": "3c3810516052f0c0c54ff06357c53dba",
+    "url": "/faezemoradi8993.github.io/portfolio1/static/media/maya-1.3c381051.jpg"
+  },
+  {
+    "revision": "d9dba34336ace015f86e2c1ac1a87fde",
+    "url": "/faezemoradi8993.github.io/portfolio1/static/media/maya-3.d9dba343.jpg"
+  },
+  {
+    "revision": "8d1d9436cea0e93cb66ea0a72d1c087d",
+    "url": "/faezemoradi8993.github.io/portfolio1/static/media/maya-4.8d1d9436.jpg"
+  },
+  {
+    "revision": "328b73a7c9e740e346e441f683f2b029",
+    "url": "/faezemoradi8993.github.io/portfolio1/static/media/resume.328b73a7.jpg"
+  }
+]);
